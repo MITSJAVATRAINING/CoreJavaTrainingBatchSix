631d552969ea5e18ccdd5f6172622614bb95ca16
Shaleen Agarwal <shaleen.agarwal@wdc.com>
2020-08-24 05:30:35 +0530

    First Commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,24 @@
+
+public class HelloWorld {
+	
+	int age = 25;
+	String name = "Shaleen";
+	
+	public static void main(String args[]) {
+		
+		int a = 10;
+		int b = 20;
+		
+		
+		int c;
+		c = a + b;
+		
+		System.out.println("the sum of a and b is " + c);
+		
+		boolean isPerson = true;
+		System.out.println("Hello World");
+		
+		System.out.println("The value of variable a and b are " + a + " and  " + b);
+	}
+
+}
